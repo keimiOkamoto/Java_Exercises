6e60b2ce0e7b6f214d5dcd94fd92a509d6d403ee
keimi <keimiokamoto@gmail.com>
2014-02-16 01:00:53 +0000

    file not printing

---
+++ day_16_input_output/ex.03_cat/src/myPackage/FileSearchMachine.java
@@ -0,0 +1,14 @@
+/**
+* Day 16 (Input and output) exercise 03 - 'cat'
+*
+* A small proram that takes a user input from the command line. If 
+* the file with the same name exists the program must show the 
+* contents on the screen.  If it doesn's exist it must say "NO",
+*/
+
+public class FileSearchMachine {
+
+	public static void main (String[] args) {
+		
+	}
+}
