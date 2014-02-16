ea944047f84385acae4958a58eca90269156359d
keimi <keimiokamoto@gmail.com>
2014-02-16 15:17:44 +0000

    IOexception working

--- day_16_input_output/ex.03_cat/src/myPackage/FileSearchMachine.java
+++ day_16_input_output/ex.03_cat/src/myPackage/FileSearchMachine.java
@@ -12,13 +12,29 @@ import java.io.*;
 
 public class FileSearchMachine {
 
+	/**
+	* User enters a file name if it exists the program will
+	* open and read the file.
+	* @param args User inputs file name.
+	*/
 	public static void main (String[] args) {
-		File path = new File("./myPackage/openMe.txt");
-
-		if (path.exists()) {
-			System.out.println("File found");
-		} else {
-			System.out.println("Not found");
+		File file = new File("." + File.separator + "myPackage" + File.separator + args[0]);
+		try {
+			if (file.exists()) {
+				BufferedReader in = new BufferedReader(new FileReader(file));
+				String line ;
+				while ((line = in.readLine()) != null) {
+					System.out.println(line);	
+				}
+			}  else {
+				throw new FileNotFoundException();
+			}
+		} catch (FileNotFoundException ex) {
+			System.out.println("ERROR: File does  not exist!");
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
+			
+		
 	}
 }
